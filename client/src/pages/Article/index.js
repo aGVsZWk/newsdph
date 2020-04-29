212df6fb366f326aa8b269a95300fe3a79f8b624
qq905713813@163.com <qq905713813@163.com>
2020-04-30 00:10:44 +0800

    modal and props-history-pushandgoback

--- client/src/pages/Article/index.js
+++ client/src/pages/Article/index.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Card, Button} from 'antd';
+import {Card, Button, Modal} from 'antd';
 import api from '@/api'
 
 class Article extends Component {
@@ -9,16 +9,40 @@ class Article extends Component {
 	}
 
 	componentWillMount() {
-		api.article.getArticles()
-		.then((value) => {console.log(value)})
-		.catch((err) => {console.log(err)})
+		api.article.getArticles().then((value) => {
+			console.log(value)
+		}).catch((err) => {
+			console.log(err)
+		})
+	}
+
+	editHandler = (record) => {
+		console.log(record);
+		Modal.confirm({
+			// title: '标题',
+			content: `删除【${record.id}】？此操作不可逆，谨慎操作！`,
+			onCancel: () => (console.log("用户取消了")),
+			onOk: () => (console.log("用户确认了"))
+		})
+	}
+	goHandler = (record) => {
+		this.props.history.push('/admin/dashboard/')
+	}
+	backHandler = (record) => {
+		this.props.history.goBack()
 	}
 	render() {
+		let record = {
+			id: 1
+		}
 		return (
-			<Card title="文章列表" extra={<Button type="dashed">导出excel</Button>}>
+			<Card title="文章列表" extra={<Button type = "dashed" > 导出excel</Button>}>
 				<p>Card content</p>
 				<p>Card content</p>
 				<p>Card content</p>
+				<Button onClick={this.editHandler.bind(this, record)}>Modal</Button>
+				<Button onClick={this.goHandler.bind(this, record)}>Go</Button>
+				<Button onClick={this.backHandler.bind(this, record)}>Back</Button>
 			</Card>
 
 		)
